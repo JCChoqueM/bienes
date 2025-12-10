a0bf10a91687fdc7b2a1b8b724653e3050631ff4
Julio Cesar Choque Mamani <122507585+JCChoqueM@users.noreply.github.com>
2025-12-10 05:29:54 -0400

    dividenso

--- views/layout.php
+++ views/layout.php
@@ -1,23 +1,25 @@
 <?php
-    if(!isset($_SESSION)) {
-        session_start();
-    }
-    $auth = $_SESSION['login'] ?? false;
+if (!isset($_SESSION)) {
+    session_start();
+}
+$auth = $_SESSION['login'] ?? false;
 
-    if(!isset($inicio)) {
-        $inicio = false;
-    } 
+if (!isset($inicio)) {
+    $inicio = false;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienes Raices</title>
+    <title>nuevos dividendos</title>
     <link rel="stylesheet" href="../build/css/app.css">
 </head>
+
 <body>
-    
+
     <header class="header <?php echo $inicio  ? 'inicio' : ''; ?>">
         <div class="contenedor contenido-header">
             <div class="barra">
@@ -36,15 +38,15 @@
                         <a href="/propiedades">Anuncios</a>
                         <a href="/blog">Blog</a>
                         <a href="/contacto">Contacto</a>
-                        <?php if($auth): ?>
+                        <?php if ($auth): ?>
                             <a href="/logout">Cerrar Sesión</a>
                         <?php endif; ?>
                     </nav>
                 </div>
-                
+
             </div> <!--.barra-->
 
-            <?php  echo $inicio ? "<h1>Venta de Casas y Departamentos Exclusivos de Lujo</h1>" : ''; ?>
+            <?php echo $inicio ? "<h1>Venta de Casas y Departamentos Exclusivos de Lujo</h1>" : ''; ?>
         </div>
     </header>
 
@@ -67,4 +69,5 @@
 
     <script src="/build/js/bundle.min.js"></script>
 </body>
+
 </html>
